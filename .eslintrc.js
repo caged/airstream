279cf52aa82083c3934e3552ef81edd648475d88
Justin Palmer <encytemedia@gmail.com>
2020-01-17 17:12:21 -0800

    use webpack compilation

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    figma: 'readonly',
+    __html__: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
+  },
+};
--- src/code.ts
+++ src/code.ts
@@ -1,5 +1,5 @@
-import { color, rgb } from "d3-color";
-import { scaleLinear } from "d3-scale";
+import { scaleLinear } from 'd3-scale';
+
 figma.showUI(__html__);
 
 enum AllowedTypes {
@@ -10,24 +10,26 @@ enum AllowedTypes {
 
 const scale = scaleLinear().domain([0, 0.4, 0.8]);
 
-const getFirstFill = shape => {
+const getFirstFill = (shape) => {
   const solids = (shape as GeometryMixin).fills as ReadonlyArray<Paint>;
-  return solids.find(s => s.type === "SOLID") as SolidPaint;
+  return solids.find((s) => s.type === 'SOLID') as SolidPaint;
 };
 
-const isAllowedShape = (shape): boolean => {
-  return (AllowedTypes[shape.type] as unknown) <= AllowedTypes.POLYGON;
-};
+const isAllowedShape = (shape): boolean => (
+  (AllowedTypes[shape.type] as unknown) <= AllowedTypes.POLYGON
+);
+
+const clone = (val) => JSON.parse(JSON.stringify(val));
 
 const generateColorSteps = () => {
   const { selection } = figma.currentPage;
   const fills = [];
-  for (let shape of selection) {
+  for (const shape of selection) {
     if (isAllowedShape(shape)) {
       const fill = getFirstFill(shape);
       fills.push({
         name: shape.name,
-        color: fill.color
+        color: fill.color,
       });
     }
   }
@@ -35,12 +37,12 @@ const generateColorSteps = () => {
   return fills;
 };
 
-figma.ui.onmessage = msg => {
+figma.ui.onmessage = (msg) => {
   const size = 50;
-  if (msg.type === "generate-steps") {
+  if (msg.type === 'generate-steps') {
     const nodes = [];
-    const fills = generateColorSteps();
-    scale.range(fills.map(f => f.color));
+    const steps = generateColorSteps();
+    scale.range(steps.map((f) => f.color));
 
     let j = 0;
     for (let i = 0; i <= 0.8; i += 0.1) {
@@ -48,7 +50,9 @@ figma.ui.onmessage = msg => {
       const rect = figma.createRectangle();
       rect.resize(size, size);
       rect.x = j * size + (j * size) / 3;
-      rect.fills = [{ type: "SOLID", color }];
+      const fills = clone(rect.fills);
+      fills[0].color = color;
+      rect.fills = fills;
       figma.currentPage.appendChild(rect);
       nodes.push(rect);
       j += 1;
