19cc38ba6d6c1445893467139d9de562ac62fe80
Justin Palmer <encytemedia@gmail.com>
2020-01-23 09:45:45 -0800

    parent append button isn't working as ref.  just stash this away for now and come back to it later

--- src/components/ColorFillArrayInput.tsx
+++ src/components/ColorFillArrayInput.tsx
@@ -1,36 +1,56 @@
 import * as React from 'react'
-import { useFormContext } from 'react-hook-form'
+import { useFormContext, useFieldArray } from 'react-hook-form'
 
 interface ColorFillArrayProps {
   name: string
+  appendRef: React.MutableRefObject<any>
 }
 
-function ColorFillArrayInput({ name }: ColorFillArrayProps) {
-  const [indexes, setIndexes] = React.useState([])
-  const [counter, setCounter] = React.useState(0)
-  const { register } = useFormContext()
+function ColorFillArrayInput({ name, appendRef }: ColorFillArrayProps) {
+  const { register, control, formState, ...other } = useFormContext()
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name,
+  })
 
-  const addColor = () => {
-    setIndexes((prevIndexes) => [...prevIndexes, counter])
-    setCounter((prevCounter) => prevCounter + 1)
-  }
+  console.log(appendRef)
 
-  const removeColor = (index) => () => {
-    setIndexes((prevIndexes) => [
-      ...prevIndexes.filter((item) => item !== index),
-    ])
-    setCounter((prevCounter) => prevCounter - 1)
-  }
+  // appendRef && appendRef.current && appendRef.current.onClick = () => {
+  //   console.log('clicking');
+
+  //   append({ color: '#00ff00' })
+  // }
+
+  // const [indexes, setIndexes] = React.useState([])
+  // const [counter, setCounter] = React.useState(0)
+  // const { register } = useFormContext()
+
+  // const addColor = () => {
+  //   setIndexes((prevIndexes) => [...prevIndexes, counter])
+  //   setCounter((prevCounter) => prevCounter + 1)
+  // }
+
+  // const removeColor = (index) => () => {
+  //   setIndexes((prevIndexes) => [
+  //     ...prevIndexes.filter((item) => item !== index),
+  //   ])
+  //   setCounter((prevCounter) => prevCounter - 1)
+  // }
+  console.log(fields, !formState.dirty)
 
   return (
     <div>
-      <button onClick={addColor}>add +</button>
-      {indexes.map((index) => {
+      test
+      {fields.map((item, index) => {
         const fieldName = `${name}[${index}]`
         return (
-          <div key={fieldName}>
-            <input name={`${fieldName}.color`} ref={register} />
-            <button onClick={removeColor(index)}>remove</button>
+          <div key={item.id}>
+            <input
+              name={`${fieldName}.color`}
+              defaultValue={item.color}
+              ref={register}
+            />
+            <button onClick={remove(index)}>remove</button>
           </div>
         )
       })}
--- src/components/SwatchTransitionComponent.tsx
+++ src/components/SwatchTransitionComponent.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { useForm, useFieldArray, FormContext } from 'react-hook-form'
+import { useForm, FormContext } from 'react-hook-form'
 import ColorFillArrayInput from './ColorFillArrayInput'
 
 const SwatchTransitionComponent: React.FC = () => {
@@ -11,37 +11,37 @@ const SwatchTransitionComponent: React.FC = () => {
     },
   })
 
-  const { handleSubmit, setValue, register, control, errors } = methods
+  const { handleSubmit, register } = methods
 
-  const { fields, append, remove } = useFieldArray({
-    control,
-    name: 'fill',
-  })
+  // const { fields, append, remove } = useFieldArray({
+  //   control,
+  //   name: 'fill',
+  // })
 
   const onSubmit = (data) => {
     parent.postMessage({ pluginMessage: { data } }, '*')
   }
 
-  const handleChange = (event) => {
-    console.log('changing')
+  // const handleChange = (event) => {
+  //   console.log('changing')
 
-    const { type, value, dataset } = event.target
-    const index = dataset.index
-    const isColorChange = type === 'color'
-    const name = isColorChange ? `fill[${index}].name` : `fill[${index}].color`
+  //   const { type, value, dataset } = event.target
+  //   const index = dataset.index
+  //   const isColorChange = type === 'color'
+  //   const name = isColorChange ? `fill[${index}].name` : `fill[${index}].color`
 
-    if (isColorChange || value.length > 5) {
-      const val = (isColorChange
-        ? value.replace(/#/, '')
-        : `#${value}`
-      ).toUpperCase()
-      setValue(name, val)
-    }
-  }
+  //   if (isColorChange || value.length > 5) {
+  //     const val = (isColorChange
+  //       ? value.replace(/#/, '')
+  //       : `#${value}`
+  //     ).toUpperCase()
+  //     setValue(name, val)
+  //   }
+  // }
 
-  const handleFocus = (event) => {
-    event.target.select()
-  }
+  // const handleFocus = (event) => {
+  //   event.target.select()
+  // }
 
   const focusInput = (event) => {
     const { target } = event
@@ -58,14 +58,25 @@ const SwatchTransitionComponent: React.FC = () => {
     event.target.parentNode.classList.remove('focused')
   }
 
+  interface RefButtonProps {
+    children: React.ReactNode
+    className: string
+  }
+
+  const RefButton = React.forwardRef(function RefButtonFunc(
+    props: RefButtonProps,
+    ref: React.LegacyRef<HTMLButtonElement>
+  ) {
+    return <button ref={ref}>{props.children}</button>
+  })
+
+  const ref = React.createRef<HTMLButtonElement>()
+
   return (
     <div className="plugin-body">
       <div className="flex">
         <h2 className="section-title flex-1">Swatch Blend</h2>
-        <button
-          className="btn-icon plus"
-          onClick={() => append({ color: defaultColor })}
-        >
+        <RefButton className="btn-icon plus" ref={ref}>
           <svg height="12" width="12">
             <line
               x1="0"
@@ -90,89 +101,36 @@ const SwatchTransitionComponent: React.FC = () => {
               }}
             />
           </svg>
-        </button>
+        </RefButton>
       </div>
       <p className="info">
         Create a blended palette from a list of fills using the given number of
         steps.
       </p>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="form-row">
-          <div className="icon-input" onClick={focusInput}>
-            <label htmlFor="steps">Steps</label>
-            <input
-              className="steps ml-1 mr-2"
-              name="steps"
-              type="number"
-              min={2}
-              max={99}
-              ref={register({ required: true })}
-              defaultValue={3}
-              onBlur={unfocusInput}
-            />
+      <FormContext {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-row">
+            <div className="icon-input" onClick={focusInput}>
+              <label htmlFor="steps">Steps</label>
+              <input
+                className="steps ml-1 mr-2"
+                name="steps"
+                type="number"
+                min={2}
+                max={99}
+                ref={register({ required: true })}
+                defaultValue={3}
+                onBlur={unfocusInput}
+              />
+            </div>
           </div>
-        </div>
-        {/* <div className="form-row">
-            <ColorFillArrayInput name="fill" />
-          </div> */}
-        <div className="form-row">
-          {fields.map((item, index) => {
-            const name = `fill[${index}]`
-
-            return (
-              <div key={item.id} className="form-row-item">
-                <div className="flex-1">
-                  <div className="color-input">
-                    <input
-                      type="color"
-                      name={`${name}.color`}
-                      ref={register}
-                      onChange={handleChange}
-                      defaultValue={item.color}
-                      data-index={index}
-                    />
-                    <input
-                      type="text"
-                      size={7}
-                      defaultValue={item.color.replace(/#/, '').toUpperCase()}
-                      ref={register}
-                      name={`${name}.name`}
-                      onChange={handleChange}
-                      onFocus={handleFocus}
-                      data-index={index}
-                      maxLength={6}
-                    />
-                  </div>
-                </div>
-                <div className="actions flex-shrink">
-                  {fields.length > 2 && index >= 2 && (
-                    <button
-                      className="btn-icon minus"
-                      onClick={() => remove(index)}
-                    >
-                      <svg height="1" width="12">
-                        <line
-                          x1="0"
-                          y1="0"
-                          x2="12"
-                          y2="0"
-                          style={{
-                            lineWidth: 1,
-                            stroke: '#111111',
-                            shapeRendering: 'crisp-edges',
-                          }}
-                        />
-                      </svg>
-                    </button>
-                  )}
-                </div>
-              </div>
-            )
-          })}
-        </div>
-        <input type="submit" value="Submit" className="btn-primary" />
-      </form>
+          <div className="form-row">
+            <ColorFillArrayInput name="fill" appendRef={ref} />
+          </div>
+          <input type="submit" value="Submit" className="btn-primary" />
+        </form>
+      </FormContext>
     </div>
   )
 }
