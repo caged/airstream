ad2f26dfa381476fcef4ce9f097c921e06818550
Justin Palmer <encytemedia@gmail.com>
2020-01-23 08:16:16 -0800

    working version retaining color fill state

--- src/components/SwatchTransitionComponent.tsx
+++ src/components/SwatchTransitionComponent.tsx
@@ -23,6 +23,8 @@ const SwatchTransitionComponent: React.FC = () => {
   }
 
   const handleChange = (event) => {
+    console.log('changing')
+
     const { type, value, dataset } = event.target
     const index = dataset.index
     const isColorChange = type === 'color'
@@ -95,82 +97,82 @@ const SwatchTransitionComponent: React.FC = () => {
         steps.
       </p>
 
-      <FormContext {...methods}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="form-row">
-            <div className="icon-input" onClick={focusInput}>
-              <label htmlFor="steps">Steps</label>
-              <input
-                className="steps ml-1 mr-2"
-                name="steps"
-                type="number"
-                min={2}
-                max={99}
-                ref={register({ required: true })}
-                defaultValue={3}
-                onBlur={unfocusInput}
-              />
-            </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-row">
+          <div className="icon-input" onClick={focusInput}>
+            <label htmlFor="steps">Steps</label>
+            <input
+              className="steps ml-1 mr-2"
+              name="steps"
+              type="number"
+              min={2}
+              max={99}
+              ref={register({ required: true })}
+              defaultValue={3}
+              onBlur={unfocusInput}
+            />
           </div>
-          <div className="form-row">
+        </div>
+        {/* <div className="form-row">
             <ColorFillArrayInput name="fill" />
-          </div>
-          <div className="form-row">
-            {/* {fields.map((item, index) => {
-              return (
-                <div key={item.id} className="form-row-item">
-                  <div className="flex-1">
-                    <div className="color-input">
-                      <input
-                        type="color"
-                        name={`fill[${index}].color`}
-                        defaultValue={item.color}
-                        ref={register}
-                        onChange={handleChange}
-                        data-index={index}
-                      />
-                      <input
-                        type="text"
-                        size={7}
-                        defaultValue={defaultColor.replace(/#/, '')}
-                        ref={register}
-                        name={`fill[${index}].name`}
-                        onChange={handleChange}
-                        onFocus={handleFocus}
-                        data-index={index}
-                        maxLength={6}
-                      />
-                    </div>
-                  </div>
-                  <div className="actions flex-shrink">
-                    {fields.length > 2 && index >= 2 && (
-                      <button
-                        className="btn-icon minus"
-                        onClick={() => remove(index)}
-                      >
-                        <svg height="1" width="12">
-                          <line
-                            x1="0"
-                            y1="0"
-                            x2="12"
-                            y2="0"
-                            style={{
-                              lineWidth: 1,
-                              stroke: '#111111',
-                              shapeRendering: 'crisp-edges',
-                            }}
-                          />
-                        </svg>
-                      </button>
-                    )}
+          </div> */}
+        <div className="form-row">
+          {fields.map((item, index) => {
+            const name = `fill[${index}]`
+
+            return (
+              <div key={item.id} className="form-row-item">
+                <div className="flex-1">
+                  <div className="color-input">
+                    <input
+                      type="color"
+                      name={`${name}.color`}
+                      ref={register}
+                      onChange={handleChange}
+                      defaultValue={item.color}
+                      data-index={index}
+                    />
+                    <input
+                      type="text"
+                      size={7}
+                      defaultValue={item.color.replace(/#/, '').toUpperCase()}
+                      ref={register}
+                      name={`${name}.name`}
+                      onChange={handleChange}
+                      onFocus={handleFocus}
+                      data-index={index}
+                      maxLength={6}
+                    />
                   </div>
                 </div>
-              )
-            })} */}
-          </div>
-          <input type="submit" value="Submit" className="btn-primary" />
-        </form>
-      </FormContext>
+                <div className="actions flex-shrink">
+                  {fields.length > 2 && index >= 2 && (
+                    <button
+                      className="btn-icon minus"
+                      onClick={() => remove(index)}
+                    >
+                      <svg height="1" width="12">
+                        <line
+                          x1="0"
+                          y1="0"
+                          x2="12"
+                          y2="0"
+                          style={{
+                            lineWidth: 1,
+                            stroke: '#111111',
+                            shapeRendering: 'crisp-edges',
+                          }}
+                        />
+                      </svg>
+                    </button>
+                  )}
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        <input type="submit" value="Submit" className="btn-primary" />
+      </form>
     </div>
   )
 }
