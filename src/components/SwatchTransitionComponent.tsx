a4643e0c330e6d855a3b0a4b21789753cdc25629
Justin Palmer <encytemedia@gmail.com>
2020-01-27 19:24:58 -0800

    abstract away figma input

--- src/components/SwatchTransitionComponent.tsx
+++ src/components/SwatchTransitionComponent.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
-import { useForm, useFieldArray } from 'react-hook-form'
+import { useForm, useFieldArray, FormContext } from 'react-hook-form'
 import { generateColorTransition } from '../utilities'
+import FigmaInput from './FigmaInput'
 
 interface ColorSwatchRowProps {
   item: any
@@ -49,7 +50,6 @@ const ColorSwatchRow = ({
             name={`${name}.color`}
             ref={register}
             onChange={handleColorChange}
-            defaultValue={item.color}
             data-index={index}
           />
           <input
@@ -78,14 +78,16 @@ const ColorSwatchRow = ({
 
 const SwatchTransitionComponent: React.FC = () => {
   const defaultColor = '#CCCCCC'
-  const { handleSubmit, setValue, register, control, errors } = useForm({
+  const methods = useForm({
     defaultValues: {
       fill: [{ color: defaultColor }, { color: '#4A23D8' }],
     },
   })
 
-  const { fields, append, remove } = useFieldArray({ control, name: 'fill' })
-  const colorRowProps = { register, remove, errors, setValue }
+  const { fields, append, remove } = useFieldArray({
+    control: methods.control,
+    name: 'fill',
+  })
 
   const onSubmit = (data) => {
     const { steps, fill } = data
@@ -103,78 +105,48 @@ const SwatchTransitionComponent: React.FC = () => {
     parent.postMessage({ pluginMessage }, '*')
   }
 
-  const focusAndSelectValue = (event) => {
-    const { target } = event
-    const input = target.querySelector('input')
-    target.classList.add('focused')
-
-    if (input) {
-      input.focus()
-      input.select()
-    }
-  }
-
-  const unfocusInput = (event) => {
-    event.target.parentNode.classList.remove('focused')
-  }
-
   return (
-    <div className="plugin-body">
-      <div className="flex">
-        <h2 className="section-title flex-1">Swatch Blend</h2>
-        <button
-          className="btn-icon plus"
-          onClick={() => append({ color: defaultColor })}
-        >
-          &#x0002B;
-        </button>
-      </div>
-      <p className="info">
-        Create a blended palette from a list of fills using the given number of
-        steps.
-      </p>
+    <FormContext {...methods}>
+      <div className="plugin-body">
+        <div className="flex">
+          <h2 className="section-title flex-1">Swatch Blend</h2>
+          <button
+            className="btn-icon plus"
+            onClick={() => append({ color: defaultColor })}
+          >
+            &#x0002B;
+          </button>
+        </div>
+        <p className="info">
+          Create a blended palette from a list of fills using the given number
+          of steps.
+        </p>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {errors && errors.steps && errors.steps.message}
-        <div className="form-row">
-          <div className="icon-input" onClick={focusAndSelectValue}>
-            <label htmlFor="steps">Steps</label>
+        <form onSubmit={methods.handleSubmit(onSubmit)}>
+          <div className="form-row">
+            <FigmaInput name="steps" type="number" defaultValue={3} />
+          </div>
+          <div className="form-row">
+            {fields.map((item, index) => (
+              <ColorSwatchRow
+                key={item.id}
+                item={item}
+                index={index}
+                total={fields.length}
+                {...methods}
+              />
+            ))}
+          </div>
+          <div className="form-row primary-actions">
             <input
-              className="steps ml-1 mr-2"
-              name="steps"
-              type="number"
-              min={fields.length}
-              max={50}
-              ref={register({
-                required: true,
-                min: fields.length,
-                max: 50,
-              })}
-              defaultValue={3}
-              onBlur={unfocusInput}
+              type="submit"
+              value="Generate Swatches"
+              className="btn-primary"
             />
           </div>
-        </div>
-        <div className="form-row">
-          {fields.map((item, index) => (
-            <ColorSwatchRow
-              key={item.id}
-              item={item}
-              index={index}
-              total={fields.length}
-              {...colorRowProps}
-            />
-          ))}
-        </div>
-        <div className="form-row primary-actions">
-          <input
-            type="submit"
-            value="Generate Swatches"
-            className="btn-primary"
-          />
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </FormContext>
   )
 }
 
