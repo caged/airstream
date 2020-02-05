3b5b46b2518ffafdf0581637fc2e1c45f30265cd
Justin Palmer <encytemedia@gmail.com>
2020-02-04 16:07:21 -0800

    be explicit about interpolators so typescript does magic things and lets you know if you misspell something

--- src/components/ColorRamp.tsx
+++ src/components/ColorRamp.tsx
@@ -2,8 +2,55 @@ import * as React from 'react'
 import * as scales from 'd3-scale-chromatic'
 import { rgb } from 'd3-color'
 
+export type Interpolator =
+  // Cyclical
+  | 'interpolateRainbow'
+  | 'interpolateSinebow'
+
+  // Diverging
+  | 'interpolateSpectral'
+  | 'interpolateBrBG'
+  | 'interpolatePRGn'
+  | 'interpolatePiYG'
+  | 'interpolatePuOr'
+  | 'interpolateRdBu'
+  | 'interpolateRdGy'
+  | 'interpolateRdYlBu'
+  | 'interpolateRdYlGn'
+
+  // Multi-Hue Sequential
+  | 'interpolateTurbo'
+  | 'interpolateViridis'
+  | 'interpolateWarm'
+  | 'interpolateCividis'
+  | 'interpolateCool'
+  | 'interpolateCubehelixDefault'
+  | 'interpolateInferno'
+  | 'interpolateMagma'
+  | 'interpolatePlasma'
+  | 'interpolateBuGn'
+  | 'interpolateBuPu'
+  | 'interpolateGnBu'
+  | 'interpolateOrRd'
+  | 'interpolatePuBu'
+  | 'interpolatePuBuGn'
+  | 'interpolatePuRd'
+  | 'interpolateRdPu'
+  | 'interpolateYlGn'
+  | 'interpolateYlGnBu'
+  | 'interpolateYlOrBr'
+  | 'interpolateYlOrRd'
+
+  // Single-Hue Sequential
+  | 'interpolateBlues'
+  | 'interpolatePurples'
+  | 'interpolateGreens'
+  | 'interpolateGreys'
+  | 'interpolateOranges'
+  | 'interpolateReds'
+
 interface Props extends React.ComponentPropsWithoutRef<any> {
-  interpolator: string
+  interpolator: Interpolator
   width: number
   height: number
 }
