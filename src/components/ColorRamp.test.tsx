1c1bd8a1e6fac4458e0b67226665dab31d05e573
Justin Palmer <encytemedia@gmail.com>
2020-02-04 21:33:56 -0800

    test color ramp colors

--- src/components/ColorRamp.test.tsx
+++ src/components/ColorRamp.test.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import ColorRamp from './ColorRamp'
 
+// Fix for global type declration in jest-canvas-mock coliding with the default
+// CanvasRenderingContext2D
+// eslint-disable-next-line no-unused-vars
+import { CanvasRenderingContext2D } from '../../node_modules/jest-canvas-mock/lib/classes/CanvasRenderingContext2D'
+import { colorsFromInterpolator } from '../utilities'
+
 test('sets size and retains fixed pixel size', () => {
   const width = 500
   const height = 12
@@ -17,12 +23,20 @@ test('sets size and retains fixed pixel size', () => {
   expect(canvas).toHaveStyle({ height: `${height}px`, width: `${width}px` })
 })
 
-// test('sets an interpolator', () => {
-//   const interpolator = 'interpolateRainbow'
+test('draws canvas with colors from the given interpolator', () => {
+  const count = 256
+  const interpolator = 'interpolateRainbow'
+  const { container } = render(
+    <ColorRamp width={count} height={1} interpolator={interpolator} />
+  )
+  const canvas = container.querySelector('.palette') as HTMLCanvasElement
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+  const filledColors = ctx
+    .__getEvents()
+    .filter((d) => d.type === 'fillStyle')
+    .map((e) => e.props.value)
+  const scaleColors = colorsFromInterpolator(interpolator, count)
 
-//   const { container } = render(
-//     <ColorRamp width={256} height={1} interpolator={interpolator} />
-//   )
-//   const canvas = container.querySelector('.palette')
-
-// })
+  expect(ctx.fillRect).toBeCalledTimes(count)
+  expect(filledColors).toStrictEqual(scaleColors)
+})
