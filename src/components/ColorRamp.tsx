a655d2eb591489560bb6edb4303ef3cf55789ca1
Justin Palmer <encytemedia@gmail.com>
2020-02-04 21:35:27 -0800

    move interoplator interface and some color tools to utilities

--- src/components/ColorRamp.tsx
+++ src/components/ColorRamp.tsx
@@ -1,53 +1,6 @@
 import * as React from 'react'
-import * as scales from 'd3-scale-chromatic'
-import { rgb } from 'd3-color'
-
-export type Interpolator =
-  // Cyclical
-  | 'interpolateRainbow'
-  | 'interpolateSinebow'
-
-  // Diverging
-  | 'interpolateSpectral'
-  | 'interpolateBrBG'
-  | 'interpolatePRGn'
-  | 'interpolatePiYG'
-  | 'interpolatePuOr'
-  | 'interpolateRdBu'
-  | 'interpolateRdGy'
-  | 'interpolateRdYlBu'
-  | 'interpolateRdYlGn'
-
-  // Multi-Hue Sequential
-  | 'interpolateTurbo'
-  | 'interpolateViridis'
-  | 'interpolateWarm'
-  | 'interpolateCividis'
-  | 'interpolateCool'
-  | 'interpolateCubehelixDefault'
-  | 'interpolateInferno'
-  | 'interpolateMagma'
-  | 'interpolatePlasma'
-  | 'interpolateBuGn'
-  | 'interpolateBuPu'
-  | 'interpolateGnBu'
-  | 'interpolateOrRd'
-  | 'interpolatePuBu'
-  | 'interpolatePuBuGn'
-  | 'interpolatePuRd'
-  | 'interpolateRdPu'
-  | 'interpolateYlGn'
-  | 'interpolateYlGnBu'
-  | 'interpolateYlOrBr'
-  | 'interpolateYlOrRd'
-
-  // Single-Hue Sequential
-  | 'interpolateBlues'
-  | 'interpolatePurples'
-  | 'interpolateGreens'
-  | 'interpolateGreys'
-  | 'interpolateOranges'
-  | 'interpolateReds'
+// eslint-disable-next-line no-unused-vars
+import { Interpolator, colorsFromInterpolator } from '../utilities'
 
 // Color ramps have a fixed pixel width and heights
 const FIXED_WIDTH = 256
@@ -61,19 +14,14 @@ interface Props extends React.ComponentPropsWithoutRef<any> {
 
 const ColorRamp = ({ width, height, interpolator, ...props }: Props) => {
   const canvasRef = React.useRef(null)
-  const interpolate = scales[interpolator]
-  const n = 256
-  const colors = []
+  const count = 256
+  const colors = colorsFromInterpolator(interpolator, count)
 
   props.className = `palette ${props.className}`
 
-  for (let i = 0; i < n; ++i) {
-    colors.push(rgb(interpolate(i / (n - 1))).hex())
-  }
-
   React.useEffect(() => {
     const ctx = canvasRef.current.getContext('2d')
-    for (let i = 0; i < n; ++i) {
+    for (let i = 0; i < count; ++i) {
       ctx.fillStyle = colors[i]
       ctx.fillRect(i, 0, 1, 1)
     }
@@ -81,7 +29,6 @@ const ColorRamp = ({ width, height, interpolator, ...props }: Props) => {
 
   return (
     <canvas
-      data-testid="canvas"
       ref={canvasRef}
       width={FIXED_WIDTH}
       height={FIXED_HEIGHT}
