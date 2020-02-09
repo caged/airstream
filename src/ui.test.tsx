1922022ea6680f74e967543b0216462aa68a0b34
Justin Palmer <encytemedia@gmail.com>
2020-02-09 11:22:40 -0800

    move plugin body into its own components

--- src/ui.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import { App } from './ui'
-
-test('sets size and retains fixed pixel size', () => {
-  const { container } = render(<App />)
-})
