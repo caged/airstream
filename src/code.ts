93f9b98fe47e175919a548bbbf5f636c09099670
Justin Palmer <encytemedia@gmail.com>
2020-02-23 12:02:41 -0800

    demo on how to show different UIs for different commands

--- src/code.ts
+++ src/code.ts
@@ -1,80 +1,93 @@
-import { ColorValues } from './utilities'
+figma.showUI(__html__, {
+  height: 400,
+  width: 350,
+  visible: false,
+})
 
-figma.showUI(__html__, { height: 400, width: 350 })
-
-type AvailableActions = 'generateSwatches'
-
-interface GenerateSwatchesProps {
-  colors: Array<any>
-  size?: number
-  offsetX?: number
-  offsetY?: number
-}
-
-interface MetaProps {
-  name: string
-}
-
-interface MessageProps {
-  action: AvailableActions
-  meta: MetaProps
-  colors: Array<ColorValues>
-  size: number
-}
-
-/**
- * Naive deep clone
- */
-const clone = (val) => JSON.parse(JSON.stringify(val))
-
-/**
- * Create a Figma Rect with the given fill.
- * The way figma works, the rect will be created
- * and drawn on screen
- */
-const createRect = (
-  width: number,
-  height: number,
-  fill: SolidPaint
-): RectangleNode => {
-  const rect = figma.createRectangle()
-  const fills = clone(rect.fills)
-
-  rect.resize(width, height)
-  fills[0].color = fill
-  rect.fills = fills
-
-  return rect
-}
-
-/**
- * Generate a group of swatches with the given size and colors
- */
-const generateSwatches = ({
-  colors,
-  size = 50,
-  offsetY = 0,
-}: GenerateSwatchesProps): RectangleNode[] => {
-  return colors.map((color, i) => {
-    const rect = createRect(size, size, color.fill)
-    rect.y = offsetY
-    rect.x = i * size + (i * size) / 6
-    rect.cornerRadius = 3
-    rect.name = color.hex
-    return rect
+if (figma.command == 'ui:swatch-blend') {
+  figma.ui.postMessage({
+    command: figma.command,
+    foo: 'bar',
   })
 }
 
-const handleMessage = ({ action, ...props }: MessageProps) => {
-  if (action === 'generateSwatches') {
-    const swatches = generateSwatches(props)
-    const group = figma.group(swatches, figma.currentPage)
-    group.name = `${props.meta && props.meta.name} Swatches`
-    figma.currentPage.selection = swatches
-    figma.viewport.scrollAndZoomIntoView(swatches)
-  }
-}
+// import { ColorValues } from './utilities'
 
-figma.ui.onmessage = handleMessage
+// figma.showUI(__html__, { height: 400, width: 350 })
 
-export { generateSwatches, handleMessage }
+// type AvailableActions = 'generateSwatches'
+
+// interface GenerateSwatchesProps {
+//   colors: Array<any>
+//   size?: number
+//   offsetX?: number
+//   offsetY?: number
+// }
+
+// interface MetaProps {
+//   name: string
+// }
+
+// interface MessageProps {
+//   action: AvailableActions
+//   meta: MetaProps
+//   colors: Array<ColorValues>
+//   size: number
+// }
+
+// /**
+//  * Naive deep clone
+//  */
+// const clone = (val) => JSON.parse(JSON.stringify(val))
+
+// /**
+//  * Create a Figma Rect with the given fill.
+//  * The way figma works, the rect will be created
+//  * and drawn on screen
+//  */
+// const createRect = (
+//   width: number,
+//   height: number,
+//   fill: SolidPaint
+// ): RectangleNode => {
+//   const rect = figma.createRectangle()
+//   const fills = clone(rect.fills)
+
+//   rect.resize(width, height)
+//   fills[0].color = fill
+//   rect.fills = fills
+
+//   return rect
+// }
+
+// /**
+//  * Generate a group of swatches with the given size and colors
+//  */
+// const generateSwatches = ({
+//   colors,
+//   size = 50,
+//   offsetY = 0,
+// }: GenerateSwatchesProps): RectangleNode[] => {
+//   return colors.map((color, i) => {
+//     const rect = createRect(size, size, color.fill)
+//     rect.y = offsetY
+//     rect.x = i * size + (i * size) / 6
+//     rect.cornerRadius = 3
+//     rect.name = color.hex
+//     return rect
+//   })
+// }
+
+// const handleMessage = ({ action, ...props }: MessageProps) => {
+//   if (action === 'generateSwatches') {
+//     const swatches = generateSwatches(props)
+//     const group = figma.group(swatches, figma.currentPage)
+//     group.name = `${props.meta && props.meta.name} Swatches`
+//     figma.currentPage.selection = swatches
+//     figma.viewport.scrollAndZoomIntoView(swatches)
+//   }
+// }
+
+// figma.ui.onmessage = handleMessage
+
+// export { generateSwatches, handleMessage }
--- src/ui.tsx
+++ src/ui.tsx
@@ -7,4 +7,7 @@ import './ui.css'
 // eslint-disable-next-line
 declare function require(path: string): any
 
-ReactDOM.render(<Plugin />, document.getElementById('app'))
+window.onmessage = (event) => {
+  const { pluginMessage: props } = event.data
+  ReactDOM.render(<Plugin {...props} />, document.getElementById('app'))
+}
