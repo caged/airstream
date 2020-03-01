ef9679fa4776dbea02eba12391da713a8473da9f
Justin Palmer <encytemedia@gmail.com>
2020-03-01 14:35:40 -0800

    structure our bank of figma actions so we can just define them and make them availiable

--- src/code.ts
+++ src/code.ts
@@ -1,27 +1,131 @@
+type AvailableActions = 'generateSwatches'
+
+interface GenerateSwatchesProps {
+  colors: Array<any>
+  size?: number
+  offsetX?: number
+  offsetY?: number
+}
+
+interface MetaProps {
+  name: string
+}
+
+interface MessageProps {
+  action: AvailableActions
+  meta: MetaProps
+  colors: Array<{}>
+  size: number
+}
+
 figma.showUI(__html__, { visible: false })
 
 if (figma.command) {
-  const [part, action] = figma.command.split(':')
+  const [part, view] = figma.command.split(':')
 
   if (part === 'view') {
-    figma.ui.postMessage({ view: action })
+    figma.ui.postMessage({ view })
   } else {
-    exec(action)
+    exec({ command: view })
   }
 }
 
-function exec(action) {
-  console.log('executing', action)
+/**
+ * Naive deep clone
+ */
+const clone = (val) => JSON.parse(JSON.stringify(val))
+
+/**
+ * Create a Figma Rect with the given fill.
+ * The way figma works, the rect will be created
+ * and drawn on screen
+ */
+const createRect = (
+  width: number,
+  height: number,
+  fill: SolidPaint
+): RectangleNode => {
+  const rect = figma.createRectangle()
+  const fills = clone(rect.fills)
+
+  rect.resize(width, height)
+  fills[0].color = fill
+  rect.fills = fills
+
+  return rect
 }
 
-figma.ui.onmessage = (message) => {
-  const { command } = message
+/**
+ * Generate a group of swatches with the given size and colors
+ */
+const generateSwatches = ({
+  colors,
+  size = 50,
+  offsetY = 0,
+}: GenerateSwatchesProps): RectangleNode[] => {
+  return colors.map((color, i) => {
+    const rect = createRect(size, size, color.fill)
+    rect.y = offsetY
+    rect.x = i * size + (i * size) / 6
+    rect.cornerRadius = 3
+    rect.name = color.hex
+    return rect
+  })
+}
 
-  if (command === 'resize') {
-    const { width, height } = message
+// Run ui-less commands directly from plugin.
+function exec({ command }) {
+  console.log('executing', command)
+}
+
+class AirStreamPlugin {
+  static defaults = {
+    constrainable: {
+      width: 50,
+      height: 50,
+      offset: {
+        x: 10,
+        y: 10,
+      },
+    },
+  }
+
+  static resize({ width, height }) {
     figma.ui.resize(width, height)
     figma.ui.show()
-  } else {
-    // figma.closePlugin()
   }
+
+  static generateSwatches({
+    colors,
+    width = this.defaults.constrainable.width,
+    height = this.defaults.constrainable.height,
+    offsetX = this.defaults.constrainable.offset.x,
+    offsetY = this.defaults.constrainable.offset.y,
+  }) {
+    colors.map((color, i) => {
+      const rect = createRect(width, height, color.fill)
+      rect.y = offsetY
+      rect.x = i * width + (i * width) / 6
+      rect.cornerRadius = 3
+      rect.name = color.hex
+      return rect
+    })
+  }
+
+  run(action, props) {
+    if (AirStreamPlugin[action]) {
+      AirStreamPlugin[action](props)
+    } else {
+      throw new Error(
+        `AirStreamPlugin doesnt know how to run action: ${action}`
+      )
+    }
+  }
+}
+
+const airstream = new AirStreamPlugin()
+
+figma.ui.onmessage = (message) => {
+  const { action, ...props } = message
+  airstream.run(action, props)
 }
