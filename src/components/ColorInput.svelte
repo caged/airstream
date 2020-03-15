42b334e5929b5e8049bc3579a1967d09c2186c0f
Justin Palmer <encytemedia@gmail.com>
2020-03-14 17:07:22 -0700

    spike out color blend

---
+++ src/components/ColorInput.svelte
@@ -0,0 +1,51 @@
+<script>
+  export let value = '#cccccc'
+</script>
+
+<div class="input flex justify-content-start">
+  <input bind:value type="color" class="color " />
+  <input type="text" bind:value class="text" />
+</div>
+
+<style>
+  .input {
+    border: 1px solid transparent;
+  }
+
+  .input:hover {
+    border: 1px solid var(--black1);
+  }
+
+  .color {
+    -webkit-appearance: none;
+    line-height: var(--line-height);
+    border: none;
+    width: 27px;
+    height: 27px;
+    margin: 2px 0 0 2px;
+  }
+
+  .color:hover {
+    border-color: #e1e1e1;
+  }
+
+  .text {
+    font-size: var(--font-size-xsmall);
+    font-weight: var(--font-weight-normal);
+    letter-spacing: var(--font-letter-spacing-neg-xsmall);
+    line-height: var(--line-height);
+    position: relative;
+    overflow: visible;
+    align-items: center;
+    width: 100%;
+    height: 30px;
+    margin: 1px 0 1px 0;
+    padding: 7px 4px 9px 7px;
+    color: var(--black8);
+    border: 1px solid transparent;
+    border-radius: var(--border-radius-small);
+    outline: none;
+    background-color: var(--white);
+    text-transform: uppercase;
+  }
+</style>
--- src/views/ColorBlend.svelte
+++ src/views/ColorBlend.svelte
@@ -1,11 +1,39 @@
 <script>
   import { createEventDispatcher, onMount } from 'svelte'
+  import ColorInput from '../components/ColorInput'
+  import {
+    Button,
+    Input,
+    IconAdjust,
+    Label,
+    Number,
+  } from 'figma-plugin-ds-svelte'
 
+  let steps = 9
+  let color
   const dispatch = createEventDispatcher()
 
   onMount(() => {
-    dispatch('resize', { width: 300, height: 500 })
+    dispatch('resize', { width: 300, height: 300 })
   })
+
+  function runPrimaryAction() {
+    // const colors = colorsFromInterpolator({
+    //   interpolator: activeInterpolator,
+    //   steps,
+    // })
+    runFigmaAction({
+      action: 'generateSwatches',
+      colors: [],
+    })
+  }
 </script>
 
-<div data-view="ColorBlend">Color Blend</div>
+<div data-view="ColorBlend">
+  <Label>Steps</Label>
+  <Number name="steps" bind:value={steps} iconName={IconAdjust} />
+  <ColorInput bind:value={color} />
+  <div class="actions flex justify-content-end p-xxsmall">
+    <Button on:click={runPrimaryAction}>Generate</Button>
+  </div>
+</div>
