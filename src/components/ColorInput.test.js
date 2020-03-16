913fc8491c94ef0df96e6c1b9b366d0614738120
Justin Palmer <encytemedia@gmail.com>
2020-03-15 22:57:19 -0700

    hey, tests are cool

---
+++ src/components/ColorInput.test.js
@@ -0,0 +1,53 @@
+import { render, fireEvent, wait, act } from '@testing-library/svelte'
+import ColorInput from './ColorInput'
+import { tick } from 'svelte';
+
+it('should render a color input', () => {
+  const { container } = render(ColorInput)
+  expect(container).toBeInTheDocument()
+});
+
+it('should set the input color', async () => {
+  const { container, component } = render(ColorInput)
+
+  const input = container.querySelector('.color')
+  await fireEvent.input(input, { target: { value: '#cc0000' } })
+
+  expect(input).toHaveAttribute('type', 'color')
+  expect(input).toHaveValue('#cc0000')
+});
+
+it('should set sync color and text inputs', async () => {
+  const { container, component } = render(ColorInput)
+  const color = container.querySelector('.color')
+  const text = container.querySelector('.text')
+
+  await fireEvent.input(color, { target: { value: '#cc0000' } })
+
+  expect(color).toHaveValue('#cc0000')
+  expect(text).toHaveValue('cc0000')
+
+  await fireEvent.input(text, { target: { value: '00cc00' } })
+  await fireEvent.blur(text)
+
+  expect(color).toHaveValue('#00cc00')
+  expect(text).toHaveValue('00cc00')
+});
+
+it('should set color when pressing enter on text', async () => {
+  const { container, component } = render(ColorInput)
+  const color = container.querySelector('.color')
+  const text = container.querySelector('.text')
+
+  await fireEvent.input(text, { target: { value: '00cc00' } })
+  await fireEvent.keyDown(text, { keyCode: 13 })
+
+  expect(color).toHaveValue('#00cc00')
+  expect(text).toHaveValue('00cc00')
+
+  await fireEvent.input(text, { target: { value: 'FF00FF' } })
+  await fireEvent.keyDown(text, { keyCode: 10 })
+
+  expect(color).not.toHaveValue('#FF00FF')
+  expect(text).toHaveValue('FF00FF')
+});
