f7106c7c52a0f89b0923507f52e6f973a758aee9
Justin Palmer <encytemedia@gmail.com>
2020-03-30 10:34:45 -0700

    test defaults for AirStream

--- src/AirStream.test.js
+++ src/AirStream.test.js
@@ -1,6 +1,12 @@
 import { render, fireEvent } from '@testing-library/svelte'
 import AirStream from './AirStream'
 
+it('should render plugin with a default view', () => {
+  const { container } = render(AirStream)
+  const view = container.querySelector('div[data-view="ChromaticSamples"]')
+  expect(view).toBeInTheDocument()
+});
+
 it('should render plugin with the given view', () => {
   const { container } = render(AirStream, { view: 'ChromaticSamples' })
   const view = container.querySelector('div[data-view="ChromaticSamples"]')
